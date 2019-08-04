b1e5aab81b8ce0456c008bb2483dd19c865f74d0
深野淳也 <fukanojunya@braindrain.jp>
2019-08-05 02:26:28 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {render} from 'react-dom';
+import  Pet  from './Pet';
+
+
+const App = () => {
+    return (
+        <div>
+            <h1 id="something-important">Adopt Me!</h1>
+            <Pet name="Luna" animal="Dog" breed="Havanese" />
+            <Pet name="Peper" animal="Bird" breed="Cocktail" />
+            <Pet name="Doink" animal="Cat" breed="Mixed" />
+        </div>
+    );
+};
+
+render(<App />, document.getElementById("root"));
+
